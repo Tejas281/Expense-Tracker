24da618f9c8ee9dc757cf76632c4262625d6f949
tejas <tejas1188245@gmail.com>
2020-12-31 16:23:34 +0530

    Completed Design Expense Tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import  {Header } from './components/Header'
+import {Balance} from './components/Balance'
+import {IncomeExponses} from './components/IncomeExponses'
+import {TransactionList} from './components/TransactionList'
+import {AddTransation} from './components/AddTransation'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header/>
+      <div className="container">
+         <Balance/>
+        <IncomeExponses/>
+        <TransactionList/>
+        <AddTransation/>
+      </div>
     </div>
   );
 }

34d4b273c1cace02c16dc7bef2405bfd7e442886
tejas <tejas1188245@gmail.com>
2020-12-31 21:33:04 +0530

    some error but this time is final project commit

--- src/Context/AppReducer.jsx
+++ src/Context/AppReducer.jsx
@@ -1,17 +1,17 @@
 export default function todo (state, action)  {
     switch(action.type) {
-      case 'DELETE_TRANSACTION':
-        return {
-          ...state,
-          transactions: state.transactions.filter(transaction => transaction.id !== action.payload)
-        }
-      case 'ADD_TRANSACTION':
-        return {
-          ...state,
-          transactions: [action.payload, ...state.transactions]
-        }
-      default:
-        return state;
-      
-    }
+        case 'DELETE_TRANSACTION':
+          return {
+            ...state,
+            transaction: state.transaction.filter(transaction => transaction.id !== action.payload)
+          }
+        case 'ADD_TRANSACTION':
+          return {
+            ...state,
+            transaction: [action.payload, ...state.transaction]
+          }
+        default:
+          return state;
+        
+      }
   }
